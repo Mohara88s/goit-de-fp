84ab679904e2dedddbd6912629de48d21278c4be
Mohara <mohara88s@gmail.com>
2026-04-11 11:26:57 +0300

    fix code & add screenshots

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # goit-de-fp
+
+Set-Content .env "AIRFLOW_UID=50000"
+
+docker compose up airflow-init
+
+docker compose up -d
+
+http://localhost:8080 Login: airflow Password: airflow
--- dags/silver_to_gold.py
+++ dags/silver_to_gold.py
@@ -1,21 +1,32 @@
-from pyspark.sql import SparkSession 
-from pyspark.sql.functions import avg, current_timestamp 
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg, current_timestamp, col, regexp_replace, expr
 
 # Створюємо SparkSession
 spark = SparkSession.builder.appName("SilverToGold").getOrCreate()
 
 # Читаємо дані з паркетних файлів для таблиць "athlete_bio" та "athlete_event_results"
-table_athlete_bio = spark.read.parquet("/tmp/silver/athlete_bio") 
-table_athlete_event_results = spark.read.parquet("/tmp/silver/athlete_event_results") 
+table_athlete_bio = spark.read.parquet("/tmp/silver/athlete_bio")
+table_athlete_event_results = spark.read.parquet(
+    "/tmp/silver/athlete_event_results")
+
+# Глибоке очищення даних
+table_athlete_bio = table_athlete_bio.withColumn(
+    "height",
+    expr("try_cast(regexp_replace(height, ',', '.') as double)")
+).withColumn(
+    "weight",
+    expr("try_cast(regexp_replace(weight, ',', '.') as double)")
+)
 
 # Виконуємо об'єднання двох DataFrame за стовпцем "athlete_id"
-expanded_table = table_athlete_event_results.drop("country_noc").join(table_athlete_bio, "athlete_id")
+expanded_table = table_athlete_event_results.drop(
+    "country_noc").join(table_athlete_bio, "athlete_id")
 
 # Агрегуємо дані: обчислюємо середні значення для зросту та ваги, додаємо поточний час
 aggregated_df = expanded_table.groupBy("sport", "medal", "sex", "country_noc").agg(
-    avg("height").alias("avg_height"),  
-    avg("weight").alias("avg_weight"),  
-    current_timestamp().alias("timestamp"),  
+    avg("height").alias("avg_height"),
+    avg("weight").alias("avg_weight"),
+    current_timestamp().alias("timestamp"),
 )
 
 # Визначаємо шлях для збереження результатів у форматі Parquet
